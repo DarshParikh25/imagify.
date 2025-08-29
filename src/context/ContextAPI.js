0fcb3323bc5d5bc0b81f7e16a161265f43b35763
Darsh Parikh <parikhdarsh121@gmail.com>
2025-08-30 02:45:02 +0530

    Developed the hero of landing page - Development Phase 2

--- src/context/ContextAPI.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-export const darkThemeContext = createContext(false);
-export const photosContext = createContext([]);
-export const filteredPhotosContext = createContext([]);
